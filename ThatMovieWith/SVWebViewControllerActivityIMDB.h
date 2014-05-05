6736e048be850e7e2850d81c417d77d165fb46d6
jayhickey <johnrhickey@gmail.com>
2014-05-05 01:49:06 -0400

    IMDB app changed to web view share item

---
+++ ThatMovieWith/SVWebViewControllerActivityIMDB.h
@@ -0,0 +1,13 @@
+//
+//  SVWebViewControllerActivityIMDB.h
+//  ThatMovieWith
+//
+//  Created by johnrhickey on 5/5/14.
+//  Copyright (c) 2014 Jay Hickey. All rights reserved.
+//
+
+#import "SVWebViewControllerActivity.h"
+
+@interface SVWebViewControllerActivityIMDB : SVWebViewControllerActivity
+
+@end
